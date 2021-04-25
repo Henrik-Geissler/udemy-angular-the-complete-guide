15ee6ae3a238f4a22eaf449f24dbad7e2db50851
Henrik Geißler <Henrik.Geissler@adesso.de>
2021-04-25 11:27:26 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
-import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component'
+import { ShoppingListComponent } from './shopping-list/shopping-list.component'
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component'
 import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component'
 import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component'
 import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component'
 import { FooterComponent } from './footer/footer.component'
-import { NavigationComponent } from './navigation/navigation.component'
+import { NavigationComponent } from './header/navigation/navigation.component'
 import { PageComponent } from './page/page.component'
 import { RecipiesComponent } from './recipies/recipies.component'
+import { ShoppingListItemComponent } from './shopping-list/shopping-list-item/shopping-list-item.component'
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { RecipiesComponent } from './recipies/recipies.component'
     NavigationComponent,
     PageComponent,
     RecipiesComponent,
+    ShoppingListItemComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
